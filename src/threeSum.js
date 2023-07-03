ca8464c8c5500ee938c3466ff6e53fb4fb50884e
Andrew Wilborn <andrewwilborn@Andrews-Air-2.attlocal.net>
2023-07-02 21:37:28 -0400

    increased memory efficiency of threeSum to allow it to run on leetcode without crashing

--- src/threeSum.js
+++ src/threeSum.js
@@ -4,26 +4,21 @@ export var threeSum = function(nums) {
     for(let i = 0; i < nums.length; i++){
         for(let j = i+1; j < nums.length; j++){
             if(map[nums[i]+nums[j]] !== undefined){ // if map has ni + nj in it
-                solutions.push([nums[i], nums[j], 0-nums[i]-nums[j]])
+                let triplet = [nums[i], nums[j], 0-nums[i]-nums[j]]
+                triplet = triplet.sort((a,b)=>a-b)
+                let isDuplicate = false;
+                solutions.forEach((arr) => {
+                    if(arr[0] === triplet[0] && arr[1] === triplet[1]){
+                        isDuplicate = true;
+                    }
+                })
+                if(!isDuplicate){
+                    solutions.push(triplet);
+                }
             }
             
         }
         map[0-nums[i]] = i;
     }
-    // this should correctly yield all the triplets
-    // now sort and remove duplicates
-    solutions.map((arr) => arr.sort((a,b)=>a-b))
-    let finalSolutions = []
-    solutions.forEach((arrCheck) => {
-        let isDuplicate = false;
-        finalSolutions.forEach((arrSol) => {
-            if(arrCheck[0] === arrSol[0] && arrCheck[1] === arrSol[1]){
-                isDuplicate = true;
-            }
-        })
-        if(!isDuplicate){
-            finalSolutions.push(arrCheck);
-        }
-    })
-    return finalSolutions;
+    return solutions;
 };
